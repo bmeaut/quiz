c25c9d0dd2e27a5f61a8e1d2a4ffac7fe9193ab4
Drabos Tamás <drabostamas98@gmail.com>
2020-10-28 13:33:12 +0100

    Added SignalR dependencies and classes

---
+++ src/Quiz/Hub/QuizHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Quiz.Hub
+{
+    public class QuizHub : Hub<IQuizClient>
+    {
+
+        ///TODO
+    }
+}
